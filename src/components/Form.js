e3477a084eea01070a76e11b8d02f41cdf5221a7
Makoto Ogata <ogatamakoto1976@gmail.com>
2021-03-25 23:05:32 +0900

    added weather api

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,17 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 const Form =()=> {
   const [city, setCity] = useState("");
+  const getWeather =(e)=> {
+    e.preventDefault();
+    axios.get("https://api.weatherapi.com/v1/current.json?key=b24c63cafd8146d4831124254212503&q=London&aqi=no")
+      .then(res => console.log(res))
+  }
   return (
     <form>
       <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} />
-      <button type="submit">Get Weather</button>
+      <button type="submit" onClick={getWeather}>Get Weather</button>
     </form>
   );
 };
