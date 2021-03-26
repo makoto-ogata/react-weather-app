cb12ebd790dfa9a9dceb335b861c579777bf9daa
Makoto Ogata <ogatamakoto1976@gmail.com>
2021-03-26 23:05:51 +0900

    added result contents

--- src/App.js
+++ src/App.js
@@ -7,16 +7,33 @@ import './App.css';
 
 function App() {
   const [city, setCity] = useState("");
+  const [results, setResults] = useState({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    localTime: "",
+    icon: ""
+  });
   const getWeather =(e)=> {
     e.preventDefault();
     axios.get("https://api.weatherapi.com/v1/current.json?key=b24c63cafd8146d4831124254212503&q=London&aqi=no")
-      .then(res => console.log(res))
+      .then(res => {
+        setResults({
+          country: res.data.location.country,
+          cityName: res.data.location.name,
+          temperature: res.data.current.temp_c,
+          conditionText: res.data.current.condition.text,
+          localTime: res.data.location.localtime,
+          icon: res.data.current.condition.icon
+        })
+      })
   }
   return (
     <div className="test">
       <Title />
       <Form setCity={setCity} getWeather={getWeather} />
-      <Result />
+      <Result results={results} />
     </div>
   );
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,17 @@
-const Result =()=> {
+const Result =(props)=> {
   return(
-    <h1>気象データ</h1>
+    <div>
+      {props.results.cityName && <div>{props.results.cityName}</div>}
+      {props.results.country && <div>{props.results.country}</div>}
+      {props.results.temperature && <div>{props.results.temperature}<span>℃</span></div>}
+      {props.results.conditionText &&
+        <div>
+          <img src={props.results.icon} alt="icon" />
+          <span>{props.results.conditionText}</span>
+        </div>
+      }
+      {props.results.localTime && <div>{props.results.localTime}</div>}
+    </div>
   );
 };
 
